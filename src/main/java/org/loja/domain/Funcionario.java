925afeac85b705c39e67af80da8380ccc62209ad
Thiago Almeida <thiago11contato@gmail.com>
2025-02-14 03:45:39 -0300

    init commit

--- src/main/java/org/loja/domain/Funcionario.java
+++ src/main/java/org/loja/domain/Funcionario.java
@@ -1,6 +1,5 @@
 package org.loja.domain;
 
-import org.loja.domain.Pessoa;
 
 public class Funcionario extends Pessoa {
     public Funcionario(String cpf, String nome, String endereco, String telefone){
